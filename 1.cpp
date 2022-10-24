2e03a8a3e91daf1007905dee979bb85effe8c010
Keshav Gupta <guptakeshav2509@gmail.com>
2022-10-24 13:07:18 +0530

    Adding the linear Search Functionality..

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+// Doing linear search operation here...
+
+vector<int> v1;
+int n;
+
+cout<<"Enter the count of the elements to be entered..."
+cin>>n;
+
+for (int i = 0; i < n; i++)
+{
+   int a;
+   cin>>a;
+   v1.push_back(a); 
+}
+
+
+
+    return 0;
+}
